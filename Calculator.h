56750c93b3c63867696d9f50ee01acffb1c2ed66
Mazen Nassef <mezo.nassef@gmail.com>
2025-04-04 19:03:02 +0200

    added header file for planned functions

---
+++ Calculator.h
@@ -0,0 +1,21 @@
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+double add(double a, double b);
+
+double subtract(double a, double b);
+
+double multiply(double a, double b);
+
+double divide(double a, double b); 
+
+int factorial(int n);
+
+int gcd(int a, int b);
+
+int lcm(int a, int b);
+
+int generateRandomNumber(int min, int max);
+
+#endif 
